ce24cd4963973bb33f11e47e392b9fb6c68b4814
vladislaveeeeee <roshkovlad2007@gmail.com>
2023-10-07 09:52:19 +0300

    Add project files.

---
+++ cesar_c_sharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        string str;
+        int shift;
+
+        Console.Write("Enter string: ");
+        str = Console.ReadLine();
+
+        Console.Write("Enter shifting: ");
+        shift = int.Parse(Console.ReadLine());
+
+        string alphabet = "абвгдежзиіїйклмнопрстуфхцчшщьюя";
+
+        string cesar_str = "";
+
+        foreach (char ch in str)
+        {
+            int index = alphabet.IndexOf(ch);
+            if (index != -1)
+            {
+                int newIndex = (index + shift) % alphabet.Length;
+                char newChar = alphabet[newIndex];
+                cesar_str += newChar;
+            }
+            else
+            {
+                cesar_str += ch;
+            }
+        }
+
+        Console.WriteLine("Cesar string: " + cesar_str);
+    }
+}
